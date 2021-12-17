eb1a2f0ad78abc592244fc4c8859a85617839fae
Camilo Andres Vega Agudelo <camilo.vega@globant.com>
2021-12-17 14:34:42 -0500

    Add a handleClick function to cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,18 @@
 import styles from "./Card.module.css";
 
-const Card = ({ src }) => (
-  <div className={styles.card}>
-    <div>
-      <img className={styles.front} src={src} alt="Card front" />
-      <img className={styles.back} src="/img/cover.png" alt="Card back" />
+const Card = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
+  return (
+    <div className={styles.card}>
+      <div>
+        <img className={styles.front} src={card.src} alt="Card front" />
+        <img className={styles.back} src="/img/cover.png" alt="Card back" onClick={handleClick} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
